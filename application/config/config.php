a8b2e7f2ff351228d45671338759d4c936a8902b
MdTalatMahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2018-12-08 20:21:56 +0600

    added sign in & sign up
    added sign in & sign up

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/499_Project';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
--- application/views/fail.php
+++
@@ -1,7 +0,0 @@
-<div class="container-fluid">
-	
-       <div class="row">
-       	    <div class="fail">
-       	    	<h1>something went wrong. please check your information form and try again.</h1>
-       	    </div>
-</div> 
